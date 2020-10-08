d5813beb0d4d884e98d97f05f7c27060db383600
Deepak Uppal <deepakuppal@Deepaks-MacBook-Air.local>
2020-10-07 20:46:59 -0400

    slingshot

--- Chain.js
+++ Chain.js
@@ -1,20 +1,32 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
+        this.pointB = pointB;
         World.add(world, this.chain);
     }
 
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);   
+        }
+       
     }
     
+    fly(){
+       this.chain.bodyA = null; 
+
+
+    }
+
+
+
 }
